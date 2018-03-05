3cf40f5a0cb35c631ab7c0299cd974369dbcfbb8
Brandon Johnson <bmj1985@gmail.com>
2018-03-05 10:36:08 -0700

    functioning search

---
+++ index.js
@@ -0,0 +1,200 @@
+/* es-lint-disable */
+const photographs = [
+  {
+    Title: 'Odessa, Ukraine',
+    Artist: ['Rineke Dijkstra'],
+    ConstituentID: [8139],
+    ArtistBio: ['Dutch, born 1959'],
+    Nationality: ['Dutch'],
+    BeginDate: [1959],
+    EndDate: [0],
+    Gender: ['Female'],
+    Date: 'August 4, 1993',
+    Medium: 'Chromogenic color print',
+    Dimensions: '46 3/8 x 37" (117.8 x 94 cm)',
+    CreditLine: 'Gift of Agnes Gund',
+    AccessionNumber: '613.1997',
+    Classification: 'Photograph',
+    Department: 'Photography',
+    DateAcquired: '1997-12-11',
+    Cataloged: 'Y',
+    ObjectID: 55595,
+    URL: 'http://www.moma.org/collection/works/55595',
+    ThumbnailURL:
+      'http://www.moma.org/media/W1siZiIsIjMzMDA4MyJdLFsicCIsImNvbnZlcnQiLCItcmVzaXplIDMwMHgzMDBcdTAwM2UiXV0.jpg?sha=cd2bbbc2b3512e13',
+    'Height (cm)': 117.8,
+    'Width (cm)': 94.0
+  },
+  {
+    Title: "Raoul Hague's Studio. Woodstock, New York",
+    Artist: ['Lee Friedlander'],
+    ConstituentID: [2002],
+    ArtistBio: ['American, born 1934'],
+    Nationality: ['American'],
+    BeginDate: [1934],
+    EndDate: [0],
+    Gender: ['Male'],
+    Date: '1990',
+    Medium: 'Gelatin silver print',
+    Dimensions: '8 9/16 x 12 13/16" (21.7 x 32.6 cm)',
+    CreditLine: 'Purchase',
+    AccessionNumber: '614.2000',
+    Classification: 'Photograph',
+    Department: 'Photography',
+    DateAcquired: '2000-10-12',
+    Cataloged: 'Y',
+    ObjectID: 55596,
+    URL: 'http://www.moma.org/collection/works/55596',
+    ThumbnailURL:
+      'http://www.moma.org/media/W1siZiIsIjE0OTU3MSJdLFsicCIsImNvbnZlcnQiLCItcmVzaXplIDMwMHgzMDBcdTAwM2UiXV0.jpg?sha=abb422a0da329cf4',
+    'Height (cm)': 21.7,
+    'Width (cm)': 32.6
+  },
+  {
+    Title: 'Penshurst Castle',
+    Artist: ['Roger Fenton'],
+    ConstituentID: [1847],
+    ArtistBio: ['British, 1819–1869'],
+    Nationality: ['British'],
+    BeginDate: [1819],
+    EndDate: [1869],
+    Gender: ['Male'],
+    Date: '1857',
+    Medium: 'Albumen silver print from a wet-collodion glass negative',
+    Dimensions: '3 x 2 13/16" (7.6 x 7.2 cm)',
+    CreditLine: 'Purchase',
+    AccessionNumber: '614.1941',
+    Classification: 'Photograph',
+    Department: 'Photography',
+    DateAcquired: '1941-12-10',
+    Cataloged: 'Y',
+    ObjectID: 55597,
+    URL: 'http://www.moma.org/collection/works/55597',
+    ThumbnailURL:
+      'http://www.moma.org/media/W1siZiIsIjI0NzQyMiJdLFsicCIsImNvbnZlcnQiLCItcmVzaXplIDMwMHgzMDBcdTAwM2UiXV0.jpg?sha=5c843d5b6b56a505',
+    'Height (cm)': 7.6,
+    'Width (cm)': 7.2
+  },
+  {
+    Title: 'Child in Forest',
+    Artist: ['Wynn Bullock'],
+    ConstituentID: [861],
+    ArtistBio: ['American, 1902–1975'],
+    Nationality: ['American'],
+    BeginDate: [1902],
+    EndDate: [1975],
+    Gender: ['Male'],
+    Date: '1951',
+    Medium: 'Gelatin silver print',
+    Dimensions: '7 5/8 x 9 1/2" (19.4 x 24.2 cm)',
+    CreditLine: 'Purchase',
+    AccessionNumber: '614.1976',
+    Classification: 'Photograph',
+    Department: 'Photography',
+    DateAcquired: '1976-12-08',
+    Cataloged: 'Y',
+    ObjectID: 55598,
+    URL: 'http://www.moma.org/collection/works/55598',
+    ThumbnailURL:
+      'http://www.moma.org/media/W1siZiIsIjE5ODI3NCJdLFsicCIsImNvbnZlcnQiLCItcmVzaXplIDMwMHgzMDBcdTAwM2UiXV0.jpg?sha=9070a2a99585ac31',
+    'Height (cm)': 19.4,
+    'Width (cm)': 24.2
+  },
+  {
+    Title: 'Phares de la Heve: Salle des Machines',
+    Artist: ['Unknown photographer'],
+    ConstituentID: [8595],
+    ArtistBio: [],
+    Nationality: [],
+    BeginDate: [0],
+    EndDate: [0],
+    Gender: [],
+    Date: 'c. 1880',
+    Medium: 'Collotype',
+    Dimensions: '9 3/16 x 12 13/16" (23.4 x 32.5 cm)',
+    CreditLine: 'Purchase',
+    AccessionNumber: '614.1990',
+    Classification: 'Photograph',
+    Department: 'Photography',
+    DateAcquired: '1990-12-13',
+    Cataloged: 'Y',
+    ObjectID: 55599,
+    URL: 'http://www.moma.org/collection/works/55599',
+    ThumbnailURL:
+      'http://www.moma.org/media/W1siZiIsIjIxMjIzMSJdLFsicCIsImNvbnZlcnQiLCItcmVzaXplIDMwMHgzMDBcdTAwM2UiXV0.jpg?sha=8d4a66a5af849e6f',
+    'Height (cm)': 23.4,
+    'Width (cm)': 32.5
+  },
+  {
+    Title: 'Barn, Utah',
+    Artist: ['Dorothea Lange'],
+    ConstituentID: [3373],
+    ArtistBio: ['American, 1895–1965'],
+    Nationality: ['American'],
+    BeginDate: [1895],
+    EndDate: [1965],
+    Gender: ['Female'],
+    Date: '1941',
+    Medium: 'Gelatin silver print, printed 1965',
+    Dimensions: '11 3/8 x 15 5/16" (28.9 x 38.9 cm)',
+    CreditLine: 'Purchase',
+    AccessionNumber: '614.1995',
+    Classification: 'Photograph',
+    Department: 'Photography',
+    DateAcquired: '1995-06-15',
+    Cataloged: 'N',
+    ObjectID: 55600,
+    URL: null,
+    ThumbnailURL: null,
+    'Height (cm)': 28.9,
+    'Width (cm)': 38.9
+  }
+];
+
+function filterString(string, searchTerm) {
+  if (
+    typeof string === 'string' &&
+    string
+      .replace(/-|\s/g, '')
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function filterArray(array, searchTerm) {
+  if (
+    Array.isArray(array) === true &&
+    array[0] !== undefined &&
+    array[0] !== null &&
+    typeof array[0] != 'number' &&
+    array[0]
+      .toString()
+      .replace(/-|\s/g, '')
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function search(searchTerm) {
+  return photographs.filter(photograph => {
+    return (
+      Object.values(photograph).filter(value => {
+        if (filterString(value, searchTerm) === true) {
+          return true;
+        } else if (filterArray(value, searchTerm) === true) {
+          return true;
+        }
+        return false;
+      }).length > 0
+    );
+  });
+  return searchArray;
+}
+
+search('Dijkstra');
